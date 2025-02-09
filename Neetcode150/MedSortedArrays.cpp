728ae6d93c1a82ff63695eb5e60de7ce84c69bda
Harrison Xu <hsx925@gmail.com>
2025-02-09 16:21:47 -0500

    adding timemap

---
+++ Neetcode150/MedSortedArrays.cpp
@@ -0,0 +1,16 @@
+/* ----------------------------------------------------- */
+/* MedSortedArrays.cpp                                   */
+/* Author: Harrison Xu                                   */
+/* Email: hx2004@princeton.edu                           */
+/* ----------------------------------------------------- */
+
+/* LeetCode Problem: Median of Two Sorted Arrays */
+
+class Solution
+{
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2)
+    {
+        int half = (nums1.size() + nums2.size()) / 2;
+    }
+}
--- Neetcode150/TimeMap.cpp
+++ Neetcode150/TimeMap.cpp
@@ -9,21 +9,44 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <map>
 
 using namespace std;
 
 class TimeMap
 {
 public:
+    unordered_map<string, map<int, string>> keys;
     TimeMap()
     {
     }
 
     void set(string key, string value, int timestamp)
     {
+        keys[key].insert({timestamp, value});
     }
 
     string get(string key, int timestamp)
     {
+        auto it = keys[key].upper_bound(timestamp);
+        if (it == keys[key].begin())
+        {
+            return "";
+        }
+        else
+        {
+            return prev(it)->second;
+        }
     }
 };
+
+int main()
+{
+    TimeMap timemap = TimeMap();
+    timemap.set("alice", "happy", 1);
+    cout << timemap.get("alice", 1) << endl;
+    cout << timemap.get("alice", 2) << endl;
+    timemap.set("alice", "sad", 3);
+    cout << timemap.get("alice", 3) << endl;
+}
