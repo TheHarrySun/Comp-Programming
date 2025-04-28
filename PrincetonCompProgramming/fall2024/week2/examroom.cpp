137e1f2679ea7231f9308c0073e4b192fc97024e
Harrison Xu <hsx925@gmail.com>
2025-04-28 05:25:15 -0400

    adding comments for problem descriptions

--- PrincetonCompProgramming/fall2024/week2/examroom.cpp
+++ PrincetonCompProgramming/fall2024/week2/examroom.cpp
@@ -1,3 +1,28 @@
+/*
+Problem Link: https://codeforces.com/group/hNnRWqFua0/contest/551614/problem/A
+
+Description:
+It's 2050, and there are n students enrolled in COS126. As such, the
+logistics of organizing the midterm exams for the class have become
+really hard. This year, the n students will sit side by side on chairs
+arranged in a long line. Student i will be sitting on a chair that is x_i
+yards away from the beginning of the line.
+
+Because we don't want students to sit too close to each other, you have
+been tasked with determining the distance between the two closest students.
+In other words, the minimum |x_i−x_j| over all i≠j.
+
+Input
+The first line of the input contains an integer n (2 ≤ n ≤ 10^5).
+
+Then follows one line with n integers separated by spaces; the ith
+corresponds to x_i (0 ≤ x_i ≤ 10^9).
+
+Output
+The output should contain a line with a single integer, the minimum
+distance between any two students.
+*/
+
 #include <iostream>
 #include <algorithm>
 #include <vector>
--- PrincetonCompProgramming/fall2024/week2/pizzaline.cpp
+++ PrincetonCompProgramming/fall2024/week2/pizzaline.cpp
@@ -1,3 +1,31 @@
+/*
+Problem Link: https://codeforces.com/group/hNnRWqFua0/contest/551614/problem/B
+
+Description:
+It's 2050, and there are n students attending these competitive
+programming sessions. As such, the logistics of giving away pizza
+to everyone have become really hard. After a lot of discussion, the
+following process was designed. Each of the n students was assigned
+a unique number from 1 to n. Students form a long line sorted by
+their number, so student 1 goes first, then 2, and so on.
+
+The pizza has just arrived and students are lining up. However, no one
+is following the rules, and the ith student is actually the p_ith in
+the line. The unhappiness of student i is given by the number of students
+ahead of them in line that should be behind them, so the students j > i
+such that p_j < p_i. The total unhappiness of the students is the sum of
+all the student unhappinesses. Compute this value.
+
+Input
+The first line of the input contains an integer n (1 ≤ n ≤ 10^5).
+
+Then the following line contains n distinct integers between 1 and n,
+separated by spaces.
+
+Output
+The output should contain a line with a single integer, the total unhappiness.
+*/
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
