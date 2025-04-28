fe58b0d480fd15803164f61222103ef654b37011
Harrison Xu <hsx925@gmail.com>
2025-04-28 05:21:06 -0400

    class_schedule.cpp

--- PrincetonCompProgramming/fall2024/week2/class_schedule.cpp
+++ PrincetonCompProgramming/fall2024/week2/class_schedule.cpp
@@ -1,3 +1,29 @@
+/*
+Problem Link: https://codeforces.com/group/hNnRWqFua0/contest/551614/problem/C
+
+Description:
+It's 2050, and the typical Princeton undergrad now takes n classes a
+semester. Each class is given an interval [a,b], which means that
+class has a lecture starting at a hours and ending at b  hours, where
+a and b are positive integers such that a < b (note that they can
+be larger than 24; it's 2050 after all). Because it's impossible to
+schedule so many classes, there might be overlap between classes.
+
+You are given the collection of classes of a certain student. Your
+goal is to output the total amount of hours of work, i.e., how many
+hours have at least one class going on.
+
+Input
+The first line of the input contains one integer n (1 ≤ n ≤ 10^5).
+
+Then follow n lines, each containing two integers separated by spaces,
+representing a class interval. Each interval [a,b] satisfies
+1 ≤ a < b ≤ 10^9.
+
+Output
+The output should contain a single integer, the number of hours of work.
+*/
+
 #include <iostream>
 #include <vector>
 #include <utility>
